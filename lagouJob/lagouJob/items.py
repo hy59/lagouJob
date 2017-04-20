79e1d9ae8e992afee6dd3572b5b1f96db1fd4a1c
zealerHuang <937162905@qq.com>
2017-04-20 11:07:18 +0800

    完善scrapy大体框架

---
+++ lagouJob/lagouJob/items.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+
+# Define here the models for your scraped items
+#
+# See documentation in:
+# http://doc.scrapy.org/en/latest/topics/items.html
+
+import scrapy
+from scrapy import Field
+
+
+class LagouJobItem(scrapy.Item):
+    # define the fields for your item here like:
+    # name = scrapy.Field()
+
+    positionId = Field()
+    education = Field()
+    city = Field()
+    salary = Field()
+    industryField = Field()
+    workYear = Field()
+    companySize = Field()
+    financeStage = Field()
+    description = Field()
