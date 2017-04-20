fc34dba07f4a6ee12c9aadfdd6025dd15b1f99d0
zealerHuang <937162905@qq.com>
2017-04-20 12:48:49 +0800

    update cookies

--- lagouJob/lagouJob/spiders/lagou.py
+++ lagouJob/lagouJob/spiders/lagou.py
@@ -16,15 +16,15 @@ class LagouSpider(scrapy.Spider):
         super(LagouSpider, self).__init__()
         self.url = 'http://www.lagou.com/jobs/positionAjax.json'
         self.cookies = {
-            'user_trace_token': '20170408172550-237840596dd94ae88b379b3791a8c469',
+            'user_trace_token': '20170411181328-8fb1a66930404e16b0b924d337ae6f5d',
             'index_location_city': '%E5%85%A8%E5%9B%BD',
-            'SEARCH_ID': 'e354eea8e1b84ef891abe7071aa316e2',
-            'JSESSIONID': 'B93ACD36FC4545F3D9F5DA36E4B5BE0E',
-            'TG-TRACK-CODE': 'index_navigation',
-            'LGSID': '20170408203621-f83ee032-1c57-11e7-bece-525400f775ce',
-            'LGRID': '20170408175306-2a18769d-1c41-11e7-be1b-525400f775ce',
-            'LGUID': '20170408173849-2b42c224-1c3f-11e7-9d69-5254005c3644',
-            '_ga': 'GA1.2.502905871.1491644464',
+            'SEARCH_ID': '9e999986dd2649b898aaddc02b630626',
+            'JSESSIONID': '50495D980C9BCFBF594DA6AC14BAE855',
+            'TG-TRACK-CODE': 'search_code',
+            'LGSID': '20170420122346-2552ba5a-2581-11e7-b401-525400f775ce',
+            'LGRID': '20170420122642-8e2762fe-2581-11e7-b405-525400f775ce',
+            'LGUID': '20170411181328-81c29590-1e9f-11e7-9db9-5254005c3644',
+            '_ga': 'GA1.2.1668151603.1491905609',
             '_gat': '1'
         }
         self.pn = 1
